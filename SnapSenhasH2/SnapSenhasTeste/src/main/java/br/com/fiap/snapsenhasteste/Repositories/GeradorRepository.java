e0a88abe79a8ce0335999951beb3b164f66c3be8
Luan Sá <101113009+LVSA2004@users.noreply.github.com>
2023-04-09 13:16:01 -0300

    Delete SnapSenhasH2/SnapSenhasTeste directory

--- SnapSenhasH2/SnapSenhasTeste/src/main/java/br/com/fiap/snapsenhasteste/Repositories/GeradorRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.snapsenhasteste.Repositories;
-
-import br.com.fiap.snapsenhasteste.Models.GeradorDeSenhas;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GeradorRepository extends JpaRepository<GeradorDeSenhas, Long> {
-}
