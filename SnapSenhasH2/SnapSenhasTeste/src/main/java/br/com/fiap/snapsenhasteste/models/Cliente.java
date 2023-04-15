2df84a540ebd3033f3eb7a89de21b0d3b55bbe3b
Luan Sá <101113009+LVSA2004@users.noreply.github.com>
2023-04-15 19:04:14 -0300

    Add files via upload

---
+++ SnapSenhasH2/SnapSenhasTeste/src/main/java/br/com/fiap/snapsenhasteste/models/Cliente.java
@@ -0,0 +1,34 @@
+package br.com.fiap.snapsenhasteste.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name="tb_cliente")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
+    private Long id;
+    @Column(name = "cl_nome")
+    private String nome;
+    @Column(name = "cl_telefone")
+    private String telefone;
+    @Column(name = "cl_email")
+    private String email;
+    @Column(name = "cl_login")
+    private String login;
+    @Column(name = "cl_senha")
+    private String senha;
+
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<GeradorDeSenhas> senhas;
+}
+
